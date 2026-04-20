59ec66444a62b0327f07ad33932e32a69b622116
Ricardo062 <ricardocs553@gmail.com>
2026-04-20 23:47:40 +0100

    Atualizar documentos do chatbot

--- chatbot.py
+++ chatbot.py
@@ -16,8 +16,7 @@ from langchain_community.vectorstores import FAISS
 # =====================
 # Config
 # =====================
-BASE_DIR = Path(__file__).resolve().parent
-FOLDER_PATH = BASE_DIR / "Docs"
+FOLDER_PATH = r"C:\Users\ESDACSF\Desktop\Arquivos para chatbot"
 
 api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
@@ -195,5 +194,4 @@ def ask():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="127.0.0.1", port=5000, debug=True)
